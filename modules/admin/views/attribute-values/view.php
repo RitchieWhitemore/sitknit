cd0b03343d456800fb8daaa76ce4a56499bba7cd
RitchieWhitemore <RichiB@yandex.ru>
2019-01-15 11:12:44 +0300

    Добавлено отображение наименования в view для attribute-values

--- modules/admin/views/attribute-values/view.php
+++ modules/admin/views/attribute-values/view.php
@@ -2,11 +2,12 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\AttributeValue */
 
-$this->title = $model->good_id;
+$this->title = $model->value;
 $this->params['breadcrumbs'][] = ['label' => 'Attribute Values', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
@@ -19,18 +20,25 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Update', ['update', 'good_id' => $model->good_id, 'attribute_id' => $model->attribute_id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Delete', ['delete', 'good_id' => $model->good_id, 'attribute_id' => $model->attribute_id], [
             'class' => 'btn btn-danger',
-            'data' => [
+            'data'  => [
                 'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
+                'method'  => 'post',
             ],
         ]) ?>
     </p>
 
     <?= DetailView::widget([
-        'model' => $model,
+        'model'      => $model,
         'attributes' => [
-            'good_id',
-            'attribute_id',
+            [
+                'attribute' => 'good_id',
+                'value'     => ArrayHelper::getValue($model, 'good.title'),
+            ],
+            [
+                'attribute' => 'attribute_id',
+                'value'     => ArrayHelper::getValue($model, 'goodAttribute.name'),
+            ],
+
             'value',
         ],
     ]) ?>
