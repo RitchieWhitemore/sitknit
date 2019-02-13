d6bb75363371e9948255d32441ac9136a04fd695
RitchieWhitemore <RichiB@yandex.ru>
2019-02-13 11:49:54 +0300

    Добавил пакет del в gulpfile Добавил очистку web/node_modules перед сборкой Polymer

--- gulpfile.js
+++ gulpfile.js
@@ -8,46 +8,51 @@ var mqpacker = require("css-mqpacker");
 var cssmin = require('gulp-cssmin');
 var rename = require("gulp-rename");
 var runCmd = require('gulp-run');
+var del = require('del');
 
 var server = require("browser-sync");
 var run = require("run-sequence");
 
-gulp.task("style", function () {
+gulp.task("style", function() {
     gulp.src("src/less/style.less")
-            .pipe(plumber())
-            .pipe(sourcemaps.init())
-            .pipe(less())
-            .pipe(postcss([
-                autoprefixer({browsers: [
-                        "last 1 version",
-                        "last 2 Chrome versions",
-                        "last 2 Firefox versions",
-                        "last 2 Opera versions",
-                        "last 2 Edge versions"
-                    ]}),
-                mqpacker({
-                    sort: true
-                })
-            ]))
-
-            .pipe(sourcemaps.write('./maps'))
-            .pipe(gulp.dest("web/css"))
-            .pipe(server.reload({stream: true}));
-});
-
-gulp.task("admin-style", function () {
-    gulp.src("src/less/admin.less")
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(postcss([
-            autoprefixer({browsers: [
+            autoprefixer({
+                browsers: [
                     "last 1 version",
                     "last 2 Chrome versions",
                     "last 2 Firefox versions",
                     "last 2 Opera versions",
                     "last 2 Edge versions"
-                ]}),
+                ]
+            }),
+            mqpacker({
+                sort: true
+            })
+        ]))
+
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest("web/css"))
+        .pipe(server.reload({stream: true}));
+});
+
+gulp.task("admin-style", function() {
+    gulp.src("src/less/admin.less")
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(postcss([
+            autoprefixer({
+                browsers: [
+                    "last 1 version",
+                    "last 2 Chrome versions",
+                    "last 2 Firefox versions",
+                    "last 2 Opera versions",
+                    "last 2 Edge versions"
+                ]
+            }),
             mqpacker({
                 sort: true
             })
@@ -58,19 +63,21 @@ gulp.task("admin-style", function () {
         .pipe(server.reload({stream: true}));
 });
 
-gulp.task("gliphicons-style", function () {
+gulp.task("gliphicons-style", function() {
     gulp.src("src/less/gliphicons.less")
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(postcss([
-            autoprefixer({browsers: [
+            autoprefixer({
+                browsers: [
                     "last 1 version",
                     "last 2 Chrome versions",
                     "last 2 Firefox versions",
                     "last 2 Opera versions",
                     "last 2 Edge versions"
-                ]}),
+                ]
+            }),
             mqpacker({
                 sort: true
             })
@@ -80,38 +87,39 @@ gulp.task("gliphicons-style", function () {
         .pipe(cssmin())
         .pipe(rename('gliphicons.min.css'))
         .pipe(gulp.dest("web/css"))
-        //.pipe(server.reload({stream: true}));
+    //.pipe(server.reload({stream: true}));
 });
 
-gulp.task("build", function () {
+gulp.task("build", function() {
     gulp.src('web/css/*.css')
-        //.pipe(gulp.dest('D:/OSPanel/domains/siju-vyaju-yii/frontend/web/css/'))
-        //.pipe(cssmin())
-        //.pipe(rename('style.min.css'))
+    //.pipe(gulp.dest('D:/OSPanel/domains/siju-vyaju-yii/frontend/web/css/'))
+    //.pipe(cssmin())
+    //.pipe(rename('style.min.css'))
         .pipe(gulp.dest('web/css/'));
 
 });
 
 gulp.task("polymer-build", function() {
-   runCmd("polymer build").exec('', function() {
-       gulp.src('build/prod/node_modules/**/*.*')
-           .pipe(gulp.dest('web/node_modules/'))
-   });
+    del(['web/node_modules/**']);
+    runCmd("polymer build").exec('', function() {
+        gulp.src('build/prod/node_modules/**/*.*')
+            .pipe(gulp.dest('web/node_modules/'))
+    });
 });
 
-gulp.task("serve", ["style"], function () {
+gulp.task("serve", ["style"], function() {
     server.init({
         server: "."
     });
     gulp.watch("src/less/**/*.less", ["style"]);
     gulp.watch("*.html")
-            .on("change", server.reload);
+        .on("change", server.reload);
 });
 
-gulp.task("run", function (fn) {
+gulp.task("run", function(fn) {
     run(
-            "style",
-            fn
-            );
+        "style",
+        fn
+    );
 });
 
