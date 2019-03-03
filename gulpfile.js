c696199f9201e2509eaac59d8b0f3cdd693a2222
RitchieWhitemore <RichiB@yandex.ru>
2019-03-03 12:27:51 +0300

    Переделал сборку верстки

--- gulpfile.js
+++ gulpfile.js
@@ -14,28 +14,28 @@ var server = require("browser-sync");
 var run = require("run-sequence");
 
 gulp.task("style", function() {
-    gulp.src("src/less/style.less")
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(less())
-        .pipe(postcss([
-            autoprefixer({
-                browsers: [
-                    "last 1 version",
-                    "last 2 Chrome versions",
-                    "last 2 Firefox versions",
-                    "last 2 Opera versions",
-                    "last 2 Edge versions"
-                ]
-            }),
-            mqpacker({
-                sort: true
-            })
-        ]))
+  gulp.src("src/less/style.less")
+      .pipe(plumber())
+      .pipe(sourcemaps.init())
+      .pipe(less())
+      .pipe(postcss([
+        autoprefixer({
+          browsers: [
+            "last 1 version",
+            "last 2 Chrome versions",
+            "last 2 Firefox versions",
+            "last 2 Opera versions",
+            "last 2 Edge versions"
+          ]
+        }),
+        mqpacker({
+          sort: true
+        })
+      ]))
 
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest("web/css"))
-        .pipe(server.reload({stream: true}));
+      .pipe(sourcemaps.write('./maps'))
+      .pipe(gulp.dest("src/css"))
+      .pipe(server.reload({stream: true}));
 });
 
 gulp.task("admin-style", function() {
@@ -108,18 +108,18 @@ gulp.task("polymer-build", function() {
 });
 
 gulp.task("serve", ["style"], function() {
-    server.init({
-        server: "."
-    });
-    gulp.watch("src/less/**/*.less", ["style"]);
-    gulp.watch("*.html")
-        .on("change", server.reload);
+  server.init({
+    server: "./src"
+  });
+  gulp.watch("src/less/**/*.less", ["style"]);
+  gulp.watch("src/*.html")
+      .on("change", server.reload);
 });
 
 gulp.task("run", function(fn) {
-    run(
-        "style",
-        fn
-    );
+  run(
+      "style",
+      fn
+  );
 });
 
