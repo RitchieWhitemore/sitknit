dbfdec55cbf17feb9087becfc80be7776d9345fb
RitchieWhitemore <RichiB@yandex.ru>
2019-11-20 09:34:38 +0300

    Добавил поддержку env файлов

--- web/index.php
+++ web/index.php
@@ -3,12 +3,16 @@
 // comment out the following two lines when deployed to production
 use yii\helpers\ArrayHelper;
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
-require ('auth.php');
-
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::create(__DIR__ . '/../', '.env');
+$dotenv->load();
+
+defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG'));
+defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
+
+require('auth.php');
+
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = ArrayHelper::merge(
